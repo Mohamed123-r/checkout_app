42e9be1c7fa5d0cf6e1955bdefb6cbd51c1bf8ab
mohamed <mezoattalla123@gmail.com>
2024-07-04 15:43:35 +0300

    Add flutter_svg dependency and update CheckoutView with AppBar and leading icon

--- lib/Features/checkout/presentation/views/checkout_view.dart
+++ lib/Features/checkout/presentation/views/checkout_view.dart
@@ -1,10 +1,26 @@
+import 'package:checkout_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CheckoutView extends StatelessWidget {
   const CheckoutView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'My Cart',
+          style: Styles.style25,
+        ),
+        leading: Center(
+          child: SvgPicture.asset(
+            'assets/images/Arrow.svg',
+          ),
+        ),
+      ),
+    );
   }
 }
