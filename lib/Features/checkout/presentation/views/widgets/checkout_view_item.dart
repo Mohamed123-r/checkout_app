4b41f93756d72abe10fa7e11213adaba94b7b897
mohamed <mezoattalla123@gmail.com>
2024-07-04 16:02:56 +0300

    Add OrderInfoItem widget and update CheckoutViewBody with order details

--- lib/Features/checkout/presentation/views/widgets/checkout_view_item.dart
+++ lib/Features/checkout/presentation/views/widgets/checkout_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'order_info_item.dart';
 
 class CheckoutViewBody extends StatelessWidget {
   const CheckoutViewBody({
@@ -7,15 +8,31 @@ class CheckoutViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 10,
-        ),
-        Image.asset(
-          'assets/images/basket_image.png',
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Image.asset(
+            'assets/images/basket_image.png',
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(title:'Order Subtotal' , value:'42.97'),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(title:'Discount' , value:'0'),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(title:'Shopping' , value:'8'),
+        ],
+      ),
     );
   }
 }
+
