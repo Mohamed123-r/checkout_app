e0e2ee6c19f7b01e9c647d9c19a03b1db59fc7bc
mohamed <mezoattalla123@gmail.com>
2024-07-05 16:29:53 +0300

    Update CheckoutViewBody with TotalPrice, CustomButton, and SectionOfDetailsOrder widgets for enhanced UI

--- lib/Features/checkout/presentation/views/checkout_view.dart
+++ lib/Features/checkout/presentation/views/checkout_view.dart
@@ -1,4 +1,4 @@
-import 'package:checkout_app/Features/checkout/presentation/views/widgets/checkout_view_item.dart';
+import 'package:checkout_app/Features/checkout/presentation/views/widgets/checkout_view_body.dart';
 import 'package:checkout_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
---
+++ lib/Features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -0,0 +1,49 @@
+import 'package:checkout_app/Features/checkout/presentation/views/widgets/total_price.dart';
+import 'package:flutter/material.dart';
+import 'custom_button.dart';
+import 'section_of_details_order.dart';
+
+class CheckoutViewBody extends StatelessWidget {
+  const CheckoutViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Expanded(
+            child: Image.asset(
+              'assets/images/basket_image.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const SectionOfDetailsOrder(),
+          const Divider(
+            height: 30,
+            indent: 15,
+            endIndent: 15,
+            color: Color(0xffC7C7C7),
+            thickness: 2,
+          ),
+          const TotalPrice(
+            title: 'Total',
+            value: '50.97',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomButton(
+            onPressed: () {},
+            text: 'Complete Payment',
+          ),
+        ],
+      ),
+    );
+  }
+}
