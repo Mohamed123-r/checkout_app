14b858933aac536537da943c4764d3e79935fb95
mohamed <mezoattalla123@gmail.com>
2024-07-08 01:51:06 +0300

    "Implementing payment details body, including credit card form and custom button. Added dependency on flutter_credit_card package."

--- lib/Features/checkout/presentation/views/widgets/payment_details_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_body.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'custom_button.dart';
+import 'custom_credit_card.dart';
 import 'payment_list_view_item.dart';
 
-
-class PaymentDetailsBody extends StatelessWidget {
+class PaymentDetailsBody extends StatefulWidget {
   const PaymentDetailsBody({super.key});
 
   @override
+  State<PaymentDetailsBody> createState() => _PaymentDetailsBodyState();
+}
+
+class _PaymentDetailsBodyState extends State<PaymentDetailsBody> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          PaymentListViewItem(),
-        ],
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: CustomScrollView(slivers: [
+        const SliverToBoxAdapter(child: PaymentListViewItem()),
+        SliverToBoxAdapter(
+            child: CustomCreditCard(
+          formKey: formKey,
+          autovalidateMode: autovalidateMode,
+        )),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: CustomButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+                text: 'Pay'),
+          ),
+        ),
+      ]),
     );
   }
 }
-
-
