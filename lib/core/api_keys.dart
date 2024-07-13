1bc11a787c702aa0fe204832ab89ebd7868ec87b
mohamed <mezoattalla123@gmail.com>
2024-07-14 02:29:34 +0300

    "Implement API keys, add Stripe publishable key, and update main.dart for Stripe initialization"

--- lib/core/api_keys.dart
+++ lib/core/api_keys.dart
@@ -1,7 +1,7 @@
 class ApiKeys {
   static const String secretKey =
-
-
-
       "sk_test_51Pb75lCoBtecyn31QA8YZ3jq7DamX8wvfu6dsfmYA5tCyAEilCwlrYzYWMoXq8V2TuNQfaHZsZPT0mFivMHXuKL200OGIqkFR0";
+
+  static const String publishableKey =
+      'pk_test_51Pb75lCoBtecyn317GpkiWY28PPQHssCJUjmoOTr0GTHWcmpYzEc5LJf9gfzv3U0kVs0Cd4lUK7ZS9rHKhN9Oi5u004JL3CGXy';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:checkout_app/core/api_keys.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'Features/checkout/presentation/views/checkout_view.dart';
 
 void main() {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const CheckoutApp());
 }
 
